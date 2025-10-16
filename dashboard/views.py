76f2025876efd224a84ced4df730c53cb505374a
Diego Cirilo <dvcirilo@gmail.com>
2025-10-16 17:57:43 -0300

    update

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from blog.models import Blog, Postagem
 from blog.forms import PostagemModelForm 
 
@@ -20,22 +20,45 @@ def posts_novo(request):
         "blog": Blog.objects.first(),
     }
     if request.method == "POST":
-        print("ajaj")
-        return redirect("dashboard:posts")
+        form = PostagemModelForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("dashboard:posts")
+        else:
+            context["form"] = form
     else:
         context["form"] = PostagemModelForm()
     return render(request, "dashboard/posts_novo.html", context)
 
-
-
-
-
 def posts_detalhar(request, id_post):
-    pass
+    return render(request,
+                  "dashboard/posts_detalhar.html",
+                  {"post": get_object_or_404(Postagem, id=id_post)}
+            )
 
 def posts_editar(request, id_post):
-    pass
+    context = {
+        "blog": Blog.objects.first(),
+        "post": get_object_or_404(Postagem, id=id_post),
+    }
+    if request.method == "POST":
+        form = PostagemModelForm(request.POST, instance=context["post"])
+        if form.is_valid():
+            form.save()
+            return redirect("dashboard:posts")
+        else:
+            context["form"] = form
+    else:
+        context["form"] = PostagemModelForm(instance=context["post"])
+    return render(request, "dashboard/posts_editar.html", context)
 
 def posts_remover(request, id_post):
-    pass
-
+    context = {
+        "blog": Blog.objects.first(),
+        "post": get_object_or_404(Postagem, id=id_post),
+    }
+    if request.method == "POST":
+        context["post"].delete()
+        return redirect("dashboard:posts")
+    else:
+        return render(request, "dashboard/posts_remover.html", context)
